1284b9bb8b36bf539faa6fb8bb1a7a255a7daa56
lchristina26 <lchristina26@hotmail.com>
2015-02-03 17:39:10 -0700

    I think it's working mostly! Need to check values...

--- fft.c
+++ fft.c
@@ -4,40 +4,47 @@
    to compile enter: 'gcc -o fft fft.c -lm'   
    by Monica Thornton and Leah Thompson */
 
+#include <stdlib.h>
 #include <math.h>
 #include <stdio.h>
 #include <string.h>
 
 #define M_PI 3.14159265358979323846
 
-int* recursive_FFT(int* a);
+int* recursive_FFT(int* a, int n);
 
 int main () 
 {
     int i = 0;
     int polyA[] = {1, 2, 3};
     int* polyB;
-    polyB = recursive_FFT(polyA);
+    polyB = recursive_FFT(polyA, 3);
     
-    for (i = 0; i < 3; i++)
-        printf("polyB[i]\n");
-
+    for ( ; i < 3; i++) {
+        printf("%d\n", *polyB);
+        polyB++;
+    }
     return 0;
 }
 
-int* recursive_FFT(int* a)
+int* recursive_FFT(int* a, int n)
 {
     int i = 0, k = 0;
-    int n = sizeof(a);
+    int size = n/2;
     double w, wn;
-    static int y[sizeof(a)/2];
-    static int a0[sizeof(a)/2], a1[sizeof(a)/2];
+    int* y = (int *)malloc(size);
+    int* a0, *a1;
     int* y0;
     int* y1; // y0[sizeof(a)/2], y1[sizeof(a)/2];
-    wn = pow(2.718, ((2*M_PI)/n));
+    y0 = (int *)malloc(size);
+    y1 = (int *)malloc(size);
+    a0 = (int *)malloc(size);
+    a1 = (int *)malloc(size);
 
+    wn = pow(2.718, ((2*M_PI)/n));
     if (n == 1)
         return a;
+    
     for (i = 0; i < n; i++) {
         if (i % 2) {
             //printf("%d, %d", i, i % 2);
@@ -48,8 +55,9 @@ int* recursive_FFT(int* a)
             a1[i] = a[i];
         }
     }
-    y0 = recursive_FFT(a0);
-    y1 = recursive_FFT(a1);
+    y0 = recursive_FFT(a0, size);
+    printf("hellow");
+    y1 = recursive_FFT(a1, size);
 
     for (k = 0; k <= n/2; k++) {
         y[k] = y0[k] + (w * y1[k]);
